ca3b745cc3d277432a548b747f1f3bc069694054
danielbg-dev <danielbg.dev@gmail.com>
2018-11-15 18:46:19 -0600

    Los ultimos cambios para la entrega

--- database/init.sql
+++ database/init.sql
@@ -1,14 +1,12 @@
--- CREATE DATABASE IF NOT EXISTS pruebas;
+CREATE DATABASE IF NOT EXISTS pruebas;
 
--- use pruebas;
+use pruebas;
 
--- CREATE TABLE IF NOT EXISTS login (
---    idregistro int not null auto_increment,
---	username varchar(40),
---	password varchar(40),
-	
---	primary key(idregistro)
-	
--- );
+CREATE TABLE IF NOT EXISTS login (
+    idregistro int not null auto_increment,
+    username varchar(40),
+    password varchar(40),
+    primary key(idregistro)	
+ );
 
--- Insert into login(username, password) values('admin','9542931e640c671a60ea44a954b249c179da1239');
+ Insert into login(username, password) values('admin','9542931e640c671a60ea44a954b249c179da1239');
